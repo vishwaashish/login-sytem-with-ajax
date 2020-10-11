967117f4ee54b30f236a3897153a699f8afea5ff
Ashishkumar Vishwakarma <vishwakarmaashish165@gmail.com>
2020-10-11 17:31:57 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,14 +1,22 @@
-<?php include('db.php');?>
+<?php include('auth.php');?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php include('header.php');?>
 </head>
 <body>
-    <h1>hello</h1>
-    <a href="update_profile.php">Update profile</a>
-    <a href="logout.php">Logout</a>
+    <div class="container text-center p-5 m-5">
+        <div class=" p-5 display-1 text-center"  style="font-family: 'Sigmar One', cursive;">Hello, Sir</div>
+        <div class=" p-5 display-4 text-center"  style="font-family: 'Sigmar One', cursive;">How are you</div>
+        <div class="btn btn-primary btn-outline-primary rounded-pill btn-lg font-weight-bolder">
+            <a href="logout.php"  class="text-white text-center"  >
+            <p>LOGOUT</p>
+            </a>
+            
+        </div>
+    </div>
 </body>
 </html>
