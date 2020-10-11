0e4a101d4d1134b1b5f6cfb48c49fabaccaeec3d
Ashishkumar Vishwakarma <vishwakarmaashish165@gmail.com>
2020-10-11 19:12:30 +0530

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,3 @@
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<!-- <script src="js/popper.min.js" ></script> -->
-
-<!-- angular js -->
-<!-- <script src="js/angular.min.js" ></script> -->
